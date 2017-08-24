9dcc8c321d0df0356578e12dff066f17fb1c6716
Noon van der Silk <noonsilk@gmail.com>
2017-08-25 07:57:17 +1000

    meh

--- demo-day.sh
+++ demo-day.sh
@@ -7,10 +7,10 @@ data_dir="$1"
 out_name="$2"
 
 # # 1. Generate a gif
-# stack exec -- dance-view do-gif --video-width 1280 --video-height 720      \
-#     --source-directory $data_dir --out-file output/$out_name.gif           \
-#     --out-width 640                                                        \
-#     --fps 15
+stack exec -- dance-view do-gif --video-width 1280 --video-height 720      \
+    --source-directory $data_dir --out-file output/$out_name.gif           \
+    --out-width 640                                                        \
+    --fps 15
 
 # 2. Generate a montage
 stack exec -- dance-view do-montage --video-width 1280 --video-height 720  \
@@ -20,5 +20,5 @@ stack exec -- dance-view do-montage --video-width 1280 --video-height 720  \
     --out-width 2560
 
 # 3. Generate the json
-# stack exec -- dance-view json-export --video-width 1280 --video-height 720 \
-#     --source-directory $data_dir --out-file output/$out_name.json
+stack exec -- dance-view json-export --video-width 1280 --video-height 720 \
+    --source-directory $data_dir --out-file output/$out_name.json
